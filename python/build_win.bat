6d19e854a38a679bdd26c2c612e7f4a579d18090
jivanlembhe <jivanl@mkcl.org>
2026-09-08 17:22:30 +0530

    Add files via upload

--- python/build_win.bat
+++ python/build_win.bat
@@ -3,7 +3,7 @@ REM Builds TaskTrail.exe (single file, no console) with PyInstaller.
 REM Run this on Windows from the folder containing tasktrail.py.
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt pyinstaller
-python -m PyInstaller --noconfirm --onefile --windowed --name TaskTrail tasktrail.py
+python -m PyInstaller --noconfirm --onefile --windowed --name TaskTrail --icon icon.ico tasktrail.py
 echo.
 echo Done: dist\TaskTrail.exe
 pause
